19d68dc2de1fa682d4b921a863ef7ef59792cf8e
Nathaniel Sandler <npsandler@gmail.com>
2017-09-20 12:42:30 -0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
-  getInitialState() {
-    return {
-      origin: null,
-      destination: null
-    };
-  }
-
   render() {
     return (
       <div className="App">
@@ -21,30 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-
-        <div className="root">
-          <SearchForm>
-            <Autocomplete name="origin"
-              onPlaceChange={this.updateInput}/>
-            <Autocomplete name="destination"
-              onPlaceChange={this.updateInput}/>
-            <UnitControls {...this.state}/>
-          </SearchForm>
-          <MapComponent google={this.props.google} {...this.state}/>
-        </div>
       </div>
     );
   }
-
-
-
-  updateInput(name, latLng) {
-    let stateObj = {};
-    stateObj[name] = latLng;
-    this.setState(stateObj);
-  }
-
-
 }
 
 export default App;
