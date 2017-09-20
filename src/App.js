a20c0c3240a1cf831ddc2048e59db2d0351cfe61
Nathaniel Sandler <npsandler@gmail.com>
2017-09-20 13:10:28 -0400

    add aurportsform

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AirportsForm from './airportsForm.js'
 
 class App extends Component {
   render() {
@@ -11,8 +12,9 @@ class App extends Component {
           <h2>Welcome to Airport Calculator</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, enter two airports in the United States.
         </p>
+          <AirportsForm />
       </div>
     );
   }
---
+++ src/airportsForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class AirportsForm extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.processPledge = this.processPledge.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+    )
+  }
+
+}
+
+export default AirportsForm;
