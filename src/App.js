0f7d1cb1e8545f7a17e1762ec0eaa4165b20f5e6
Nathaniel Sandler <npsandler@gmail.com>
2017-09-20 12:35:19 -0400

    main js file

--- src/App.js
+++ src/App.js
@@ -3,19 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  getInitialState() {
+    return {
+      origin: null,
+      destination: null
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Airport Calculator</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div className="root">
+          <SearchForm>
+            <Autocomplete name="origin"
+              onPlaceChange={this.updateInput}/>
+            <Autocomplete name="destination"
+              onPlaceChange={this.updateInput}/>
+            <UnitControls {...this.state}/>
+          </SearchForm>
+          <MapComponent google={this.props.google} {...this.state}/>
+        </div>
       </div>
     );
   }
+
+
+
+  updateInput(name, latLng) {
+    let stateObj = {};
+    stateObj[name] = latLng;
+    this.setState(stateObj);
+  }
+
+
 }
 
 export default App;
