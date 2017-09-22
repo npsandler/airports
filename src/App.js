ee3590300ef5cde32e2e175f4a3fcdc45dab1e44
Nathaniel Sandler <npsandler@gmail.com>
2017-09-22 12:56:52 -0400

    add google-maps-react module

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Airport Calculator</h2>
         </div>
         <p className="App-intro">
